c1947c4e978f5d05c5e08e198ed2851ba8569770
Tung-Lam-Ha <hatunglam2412@gmail.com>
2025-07-13 19:19:35 +0100

    nothing

--- download_dataset.py
+++ download_dataset.py
@@ -3,8 +3,8 @@ from datasets import load_dataset
 
 data = load_dataset(
     "sayakpaul/nyu_depth_v2",
-    split="train[:500]",
-    cache_dir="./dataset"
+    split="train[:100]",
+    trust_remote_code=True   
 )
 
 
